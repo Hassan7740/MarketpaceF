71e3b083b3f6dd3b8d05dd0857bf04db71e0685c
YousefSayed <yousef16992@gmail.com>
2023-03-13 14:22:00 +0200

    add bootstrap and init homepage Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
