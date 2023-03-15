7fdfa91da59bad95c220976159e68792d25d201a
Hassan7740 <101173691+Hassan7740@users.noreply.github.com>
2023-03-16 01:01:48 +0200

    add product

---
+++ src/app/models/Product.ts
@@ -0,0 +1,9 @@
+import { Category } from 'src/app/models/Category';
+
+export class Product{
+        category:Category =new Category ;
+        productName:string = "" ; 
+        productDescription:string = " " ;
+        productAmount:number =  0 ; 
+        price:number = 0 ; 
+}
