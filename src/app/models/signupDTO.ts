21f0ba2b0749d36bbddc22b4b2822c5ead86df95
YousefSayed <yousef16992@gmail.com>
2023-03-15 14:41:19 +0200

    add dtos

---
+++ src/app/models/signupDTO.ts
@@ -0,0 +1,12 @@
+
+
+export class signupDTO{
+    firstName:string = "";
+    lastName:string= "";
+    gender:string= "";
+    phone:string = "";
+    email:string = "";
+    password:string = "";
+    creditCard:string = "";
+    address:any = null;
+}
